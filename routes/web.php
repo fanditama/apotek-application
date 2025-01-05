95f24dc702015a97023bea15550a48720c93e270
Affandi Pratama <afandip911@gmail.com>
2025-01-05 21:59:06 +0700

    design product details

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\ProductDetail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product/details', ProductDetail::class);
