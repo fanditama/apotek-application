7f5d1cb3db769fbcabca99258539499886546f92
Affandi Pratama <afandip911@gmail.com>
2025-01-05 17:16:43 +0700

    Design landing page

---
+++ resources/views/livewire/product-section.blade.php
@@ -0,0 +1,17 @@
+<div class="px-10 md:px-20 sm:px-30 py-3">
+    {{-- antihistamin new --}}
+    <h2 class="font-medium text-[20px] my-3">Antihistamin New</h2>
+    <livewire:product-listing />
+
+    {{-- analgesik new --}}
+    <h2 class="font-medium text-[20px] my-3">Analgesik New</h2>
+    <livewire:product-listing />
+
+    {{-- antasida new --}}
+    <h2 class="font-medium text-[20px] my-3">Antasida New</h2>
+    <livewire:product-listing />
+
+    {{-- antipiretik new --}}
+    <h2 class="font-medium text-[20px] my-3">Antipiretik New</h2>
+    <livewire:product-listing />
+</div>
