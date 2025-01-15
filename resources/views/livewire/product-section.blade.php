2c0e7272838b01622cf9f28f0a321179ed658e5e
Affandi Pratama <afandip911@gmail.com>
2025-01-15 13:33:19 +0700

    Add list category and button view all

--- resources/views/livewire/product-section.blade.php
+++ resources/views/livewire/product-section.blade.php
@@ -1,13 +1,27 @@
 <div class="px-10 md:px-20 sm:px-30 py-3">
-    {{-- antihistamin new --}}
-    <h2 class="font-medium text-[20px] my-3">Product New</h2>
-    <livewire:product-listing :category_id="0" :current_product_id="0" />
+    {{-- product new --}}
+    @include('components.navigation.view-all',[
+        'Category' => 'Brand New'
+    ])
+    <livewire:product-listing :category_id="0" :current_product_id="0"/>
 
     {{-- analgesik new --}}
-    <h2 class="font-medium text-[20px] my-3">Antihistamin</h2>
+    @include('components.navigation.view-all',[
+            'Category' => 'Antihistamin'
+    ])
     <livewire:product-listing :category_id="2" :current_product_id="0" />
 
     {{-- antasida new --}}
+    @include('components.navigation.view-all',[
+            'Category' => 'Antasida'
+    ])
     <h2 class="font-medium text-[20px] my-3">Antasida</h2>
     <livewire:product-listing :category_id="3" :current_product_id="0" />
+    
+    {{-- analgesik new --}}
+    @include('components.navigation.view-all',[
+            'Category' => 'Analgesik'
+    ])
+    <h2 class="font-medium text-[20px] my-3">Analgesik</h2>
+    <livewire:product-listing :category_id="5" :current_product_id="0" />
 </div>
